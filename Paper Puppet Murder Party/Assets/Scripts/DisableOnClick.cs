037b4838c549dda909c4d4c9272d04bebd77ab75
CocoaBeanz <86756962+CocoaBeanzz@users.noreply.github.com>
2026-02-03 00:06:51 -0800

    Credits... Sorta. Not quite working

---
+++ Paper Puppet Murder Party/Assets/Scripts/DisableOnClick.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class DisableOnClick : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
